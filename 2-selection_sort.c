5d19ab631824a3a75ad80eaeadbe54fcc5bbfc1c
Tnozone <maxnocquet@gmail.com>
2022-12-28 11:12:25 -0800

    rearrange code fix

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -10,10 +10,7 @@ void selection_sort(int *array, size_t size)
 {
 	int i, j, min, temp;
 
-	if (array == 0 || size < 2)
-		return;
-
-	for (i = 0; i < size - 1; i++)
+	for (i = 0; i < size; i++)
 	{
 		min = i;
 		for (j = i + 1; j < size; j++)
@@ -21,14 +18,14 @@ void selection_sort(int *array, size_t size)
 			if (array[j] < array[min])
 			{
 				min = j;
-				if (min != i)
-				{
-					temp = array[min];
-					array[min] = array[i];
-					array[i] = temp;
-					print_array(*array, size);
-				}
 			}
+		}
+		if (min != i)
+		{
+			temp = array[min];
+			array[min] = array[i];
+			array[i] = temp;
+			print_array(array, size);
 		}
 	}
 }
